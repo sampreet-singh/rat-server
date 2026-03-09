caed1b076a7b82d918e1d23c801077bb347a2e9d
Sampreet Singh <preet.samsam+github@gmail.com>
2026-03-10 00:03:45 +0530

    add discord bot & event bus

---
+++ src/discord/bot.ts
@@ -0,0 +1,16 @@
+import { Client, Events, GatewayIntentBits } from "discord.js";
+import config from "../../config/config.json" with { type: "json" };
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.on(Events.ClientReady, (readyClient) => {
+  console.log(`Logged in as ${readyClient.user.tag}!`);
+});
+
+client.login(config["Discord-options"]["Discord-bot-token"]);
